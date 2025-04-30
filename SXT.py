17f87901796ffc9d7406d3938777e2f032d99161
Tharki Munda <121727530+tharkimunda@users.noreply.github.com>
2025-04-30 07:50:24 +0500

    Update SXT.py

--- SXT.py
+++ SXT.py
@@ -2,6 +2,8 @@ import os, platform
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
-    import SXT64
+    chmod 777 SXT64
+    ./SXT64
 elif bit == '32bit':
-    import SXT
+    chmod 777 SXT
+    ./SXT
