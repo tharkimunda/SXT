a448bc080cda4bd2fb566d7811c5f5a9c2fd1dcc
Tharki Munda <121727530+tharkimunda@users.noreply.github.com>
2025-04-30 08:16:36 +0500

    Update SXT.py

--- SXT.py
+++ SXT.py
@@ -1,15 +1,9 @@
-import os
-import platform
-
-arch = platform.machine()
-
-if arch == "x86_64":
-    print("64-bit system detected.")
+import os, platform
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit == '64bit':
     os.system("chmod 777 SXT64")
     os.system("./SXT64")
-elif arch in ["i386", "i686"]:
-    print("32-bit system detected.")
+elif bit == '32bit':
     os.system("chmod 777 SXT")
-    os.system("./SXT")
-else:
-    print(f"Unknown architecture: {arch}")
+    os.system("SXT")
